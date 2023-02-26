ef7f4008d526b630f56983641717c8d31a5186cf
Carrington <Manyuchi>
2023-02-26 11:57:41 +0200

    Initial Commit

---
+++ CollectionView With Data Passing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView With Data Passing
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
