946a97f8f8bb59fd2596bb6bccd6d412d5f9505b
Carrington <Manyuchi>
2023-02-26 12:16:33 +0200

    Added Collection Views

---
+++ CollectionView With Data Passing/DetailsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsCollectionViewCell.swift
+//  CollectionView With Data Passing
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/02/26.
+//
+
+import UIKit
+
+class DetailsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
+    
+}
--- CollectionView With Data Passing/ViewController.swift
+++ CollectionView With Data Passing/ViewController.swift
@@ -8,7 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    var movieTitle: [String] = [
+        "Behind Enemy Lines",
+        "The Shawshank ",
+        "The Godfather (1972) ",
+        "The Lord of the Rings",
+        "The Return of the King",
+        "The Empire Strikes Back",
+        "The Matrix (1999)",
+        "Star Wars",
+        "Seven Samurai (1954)",
+        "Silence of the Lambs"
+    ]
+    
+    
+    var movieImage: [String] = [
+        "photo1", "photo2", "photo3", "photo4", "photo5", "photo7", "photo8", "photo9", "photo10","photo11"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +40,27 @@ class ViewController: UIViewController {
 
 }
 
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movieImage.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DetailsCollectionViewCell", for: indexPath) as! DetailsCollectionViewCell
+        
+        cell.movieTitleLabel.text = movieTitle[indexPath.row]
+        cell.imageView.image = UIImage(named: movieImage[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (collectionView.frame.size.width-10) / 2
+        return CGSize(width: size, height: size)
+    }
+}
